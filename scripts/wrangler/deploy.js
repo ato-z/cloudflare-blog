8e52dc47c8b7c36be8e5981efed598823be016b1
atoz <523333404@qq.com>
2023-06-19 14:07:35 +0800

    🚲 发布

---
+++ scripts/wrangler/deploy.js
@@ -0,0 +1,35 @@
+/**
+ * 发布
+ */
+const inquirer = require('inquirer');
+const shell = require('shelljs');
+const path = require('path');
+const { pathMap } = require('../helper');
+
+const { moduleList, modules } = pathMap;
+
+const deployModule = moduleName => {
+  const wranglerMain = path.join(modules, moduleName);
+  shell.exec(`cd ${wranglerMain} && wrangler deploy`);
+};
+
+const all = '🐻 ALL';
+const choices = [all, ...moduleList];
+
+inquirer
+  .prompt(
+    {
+      type: 'list',
+      name: 'moduleName',
+      message: '选择文件夹发布',
+      choices,
+    },
+    choices,
+  )
+  .then(({ moduleName }) => {
+    if (moduleName === all) {
+      moduleList.forEach(deployModule);
+    } else {
+      deployModule(moduleName);
+    }
+  });
