afdf490cddf37c9f8b3227df60d92a03e4c4bf9a
atoz <523333404@qq.com>
2023-06-19 19:50:57 +0800

    🚲 创建app容器

--- zerg/helper/createApp.ts
+++ zerg/helper/createApp.ts
@@ -1,14 +1,14 @@
 import { App, type Module } from '@ato-z/ioc';
 import { useMiddleware } from '@zerg/middleware';
 
-export const createApp = (...routers: Array<Module<new () => unknown>>) => {
+export const createApp = (...modules: Array<Module<new () => unknown>>) => {
   const app = new App();
   // 添加中间件
   useMiddleware(app);
 
-  // 导入路由列表
-  routers.forEach(router => {
-    app.use(router.install());
+  // 导入模型列表
+  modules.forEach(m => {
+    app.use(m.install());
   });
 
   return app.to;
