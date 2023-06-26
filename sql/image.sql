274a424f9b52563e3dc569e00a12201dddba0935
atoz <523333404@qq.com>
2023-06-26 16:06:16 +0800

    🛵 添加 hash thumb字段

--- sql/image.sql
+++ sql/image.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS `az_image` (
     'id' INTEGER PRIMARY KEY,
     'path' TEXT,
+    'thumb' TEXT,
+    'hash' TEXT,
     'width' INTEGER,
     'height' INTEGER,
     'size' INTEGER,
