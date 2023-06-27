c144d23a01419c12ba8258ec93585c62ed53220b
atoz <523333404@qq.com>
2023-06-27 14:23:52 +0800

    🛵 export type Master

--- zerg/modules/master/controller/v1.ts
+++ zerg/modules/master/controller/v1.ts
@@ -1,14 +1,16 @@
 import { type Context, POST, GET, Controller } from '@ato-z/ioc';
 import { LoginDto } from '../dto/Login';
 import { ModelMaster } from '@zerg/model/Master';
+import { ServiceSign } from '@zerg/service/Sign';
 
 @Controller('v1')
 export class ControllerMasterV1 {
   @POST('login') async login(ctx: Context) {
     const post = new LoginDto();
     await post.check();
-    console.log(post);
-    return post;
+    const serviceSign = new ServiceSign();
+    const sign = await serviceSign.getByLogin(post);
+    return { sign };
   }
 
   @GET('data') data() {
