0e22b6f1f1465cf765e1f86ebaf391d061bcab5c
atoz <523333404@qq.com>
2023-06-27 16:43:10 +0800

    🚲 接口文档

--- zerg/modules/master/controller/v1.ts
+++ zerg/modules/master/controller/v1.ts
@@ -5,6 +5,23 @@ import { ServiceSign } from '@zerg/service/Sign';
 
 @Controller('v1')
 export class ControllerMasterV1 {
+  /**
+   * @api {post} /master/v1/login   管理员登录获取凭证
+   * @apiVersion 1.0.0
+   * @apiName masterLogin
+   * @apiGroup master
+   *
+   * @apiHeader {String}   Content-Type application/json
+   *
+   * @apiBody {String} user       账户名
+   * @apiBody {String} password   密码
+   *
+   * @apiSuccessExample {json} 成功响应:
+   * {
+   *    // 登录成功的凭证,放置到header中可换取临时token
+   *    "sign": "string"
+   * }
+   */
   @POST('login') async login(ctx: Context) {
     const post = new LoginDto();
     await post.check();
