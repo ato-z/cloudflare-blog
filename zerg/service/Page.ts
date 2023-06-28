406f29d53a689f0fc2b2f67abda2c06b3bc9e4e7
atoz <523333404@qq.com>
2023-06-28 22:10:06 +0800

    🚲 ServicePage分页列表服务

---
+++ zerg/service/Page.ts
@@ -0,0 +1,31 @@
+import { Select } from '@ato-z/orm';
+import { PageParamDto } from '@zerg/dto';
+import { ModelBase } from '@zerg/model/Base';
+
+type SelectOption<F> = F extends (op: { where: infer P }) => any ? P : any;
+/**
+ * 文章&小计的标签
+ */
+export class ServicePage<T> {
+  constructor(
+    protected readonly model: ModelBase<T>,
+    protected readonly where: SelectOption<ModelBase<T>>,
+  ) {}
+
+  async list(pageParam: PageParamDto, field: Array<keyof T & string>) {
+    const { model, where } = this;
+    const { start, end } = pageParam;
+    const codeList = await model.select({
+      where,
+      field: field.join(','),
+      limit: [start, end],
+      order: [<keyof T & string>model.primaryKey, 'DESC'],
+    });
+    const list = await codeList.toJSON();
+    const sql = Select(model.tableName, { where, field: 'COUNT(*) AS total' });
+    const stmt = model.db.prepare(sql);
+    const total = await stmt.first('total');
+
+    return { list, total };
+  }
+}
