5245230f747e0f262b80da7c1904be9f7c782b61
atoz <523333404@qq.com>
2023-07-04 19:19:37 +0800

    🚲 切换动画

--- web/src/components/animaRouter/index.tsx
+++ web/src/components/animaRouter/index.tsx
@@ -2,9 +2,9 @@ import { motion } from 'framer-motion';
 
 export const AnimaView = ({ children }: { children: React.ReactNode }) => (
   <motion.div
-    initial={{ opacity: 0, scale: 0.5 }}
-    animate={{ opacity: 1, scale: 1 }}
-    transition={{ duration: 0.5 }}
+    initial={{ opacity: 0, x: '25%' }}
+    animate={{ opacity: 1, x: 0 }}
+    transition={{ duration: 0.3 }}
   >
     {children}
   </motion.div>
