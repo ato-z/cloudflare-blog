25eabe2348eea0e4bbfb50b93984191e4774ea86
atoz <523333404@qq.com>
2023-07-04 20:24:05 +0800

    🚲 加载动画组件

---
+++ web/src/components/loading/index.tsx
@@ -0,0 +1,21 @@
+import React, { Suspense } from 'react';
+import './index.scss';
+const Loading = () => {
+  return (
+    <div className="loader">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default ({ children }: { children: React.ReactNode }) => {
+  return <Suspense fallback={<Loading />}>{children}</Suspense>;
+};
---
+++ web/src/components/router/index.tsx
@@ -0,0 +1,69 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { RequireAuth } from '@web/pages/auth';
+import { IframeLayout } from '@web/pages/layout';
+import Loading from '../loading';
+
+const Home = lazy(() =>
+  import('../../pages/home').then(home => {
+    return new Promise(resolve => {
+      setTimeout(resolve, 10000, home);
+    });
+  }),
+);
+const Login = lazy(() => import('../../pages/login'));
+const NotFoundPage = lazy(() => import('../../pages/miss'));
+const ArticleList = lazy(() => import('../../pages/article'));
+
+export const AppRouter = () => (
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <RequireAuth>
+          <IframeLayout />
+        </RequireAuth>
+      }
+    >
+      <Route
+        path="/"
+        element={
+          <Loading>
+            <Home />
+          </Loading>
+        }
+      />
+      <Route
+        path="/article"
+        element={
+          <Loading>
+            <ArticleList />
+          </Loading>
+        }
+      />
+    </Route>
+
+    {/* 登录页面 */}
+    <Route
+      path="/login"
+      element={
+        <Loading>
+          <Login />
+        </Loading>
+      }
+    />
+
+    {/* 404 */}
+    <Route
+      path=":miss"
+      element={
+        <Loading>
+          <NotFoundPage />
+        </Loading>
+      }
+    />
+    <Route />
+  </Routes>
+);
+
+export default AppRouter;
