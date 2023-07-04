e79e2ac60910d50586a97382bf48026c6d390050
atoz <523333404@qq.com>
2023-07-04 15:27:22 +0800

    🚲 补零方法

--- web/src/helper/common.ts
+++ web/src/helper/common.ts
@@ -4,3 +4,11 @@
 export const upDocumentTitle = (title: string) => {
   document.title = title;
 };
+
+/**
+ * 补零操作
+ */
+export const fillZero = (n: number | string) => {
+  const codeN = n.toString();
+  return codeN.length > 1 ? n : '0' + n;
+};
