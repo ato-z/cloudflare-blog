35a7797e4159565da5366ecfe652fb7a4a05b738
atoz <523333404@qq.com>
2023-07-05 21:27:50 +0800

    🏍️  主题跟随时间改变

--- web/src/components/layout/component/day-time.tsx
+++ web/src/components/layout/component/day-time.tsx
@@ -12,7 +12,6 @@ const days = [am1, am2, pm1, pm2];
 export const DayTime = () => {
   const [step, setStep] = useState(0);
   const [time, setTime] = useState('');
-  const [theme, setTheme] = useTheme();
 
   const dayStepRun = useCallback(() => {
     const date = new Date();
@@ -36,17 +35,10 @@ export const DayTime = () => {
       setTime(`${fillZero(hours % 12)}:${fillZero(minutes)} AM`);
     }
 
-    // 主题
-    if (hours >= 20 && theme === 'light') {
-      setTheme('dark');
-    } else if (theme === 'dark') {
-      setTheme('light');
-    }
-
     clearTimeout(Reflect.get(window, '___dayStep'));
     const timer = setTimeout(dayStepRun, 60000);
     Reflect.set(window, '___dayStep', timer);
-  }, [setStep, setTime, time, setTheme]);
+  }, [setStep, setTime, time]);
 
   useEffect(() => {
     dayStepRun();
