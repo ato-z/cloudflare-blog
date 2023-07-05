b271dca38bc29f9aa84d37c1d8999a0c74ea2dfd
atoz <523333404@qq.com>
2023-07-05 16:51:56 +0800

    🛵 错误边界判断

--- web/src/helper/axios.ts
+++ web/src/helper/axios.ts
@@ -3,7 +3,7 @@ import { siteConfig } from '@web/config';
 
 /** axios实例 */
 export const axiosInstance = axios.create({
-  baseURL: 'http://www.baidu.com', //siteConfig.domain,
+  baseURL: siteConfig.domain,
   headers: {},
 });
 
