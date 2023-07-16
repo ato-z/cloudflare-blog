195d418003b5d1b53d14e81c2463967f21293ba1
atoz <523333404@qq.com>
2023-07-16 21:01:09 +0800

    🚲 文章状态

--- web/src/pages/article/component/status.tsx
+++ web/src/pages/article/component/status.tsx
@@ -1,13 +1,11 @@
 import { Select } from 'antd';
 
-const ArticleStatus = () => {
-  return (
-    <Select defaultValue={-1} style={{ minWidth: '100px' }}>
-      <Select.Option value={-1}>所有</Select.Option>
-      <Select.Option value={1}>发布</Select.Option>
-      <Select.Option value={0}>草稿</Select.Option>
-    </Select>
-  );
-};
+const ArticleStatus = (
+  <Select style={{ minWidth: '100px' }}>
+    <Select.Option value={-1}>所有</Select.Option>
+    <Select.Option value={1}>发布</Select.Option>
+    <Select.Option value={0}>草稿</Select.Option>
+  </Select>
+);
 
 export default ArticleStatus;
