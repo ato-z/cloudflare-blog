5feaa6d48f2b521efb753af6a2408fb9d1f07a7f
atoz <523333404@qq.com>
2023-07-16 21:02:15 +0800

    🚲 添加笔记路由

--- web/src/router/article.tsx
+++ web/src/router/article.tsx
@@ -2,10 +2,19 @@ import { lazy } from 'react';
 import { FontSizeOutlined } from '@ant-design/icons';
 
 const Article = lazy(() => import('@web/pages/article'));
+const ArticlePush = lazy(() => import('@web/pages/article/push'));
 
 export const ArticleRouter: RouteItem = {
   label: '笔记',
   icon: <FontSizeOutlined />,
   path: '/article',
   element: <Article />,
+  children: [
+    {
+      label: '添加',
+      path: 'push',
+      element: <ArticlePush />,
+      hide: true,
+    },
+  ],
 };
