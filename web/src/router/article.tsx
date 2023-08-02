acca41e9fc5153a30a7c069c5ebcd2d7d818ea90
atoz <523333404@qq.com>
2023-08-02 21:05:15 +0800

    🚲 笔记相关路由

--- web/src/router/article.tsx
+++ web/src/router/article.tsx
@@ -1,5 +1,7 @@
 import { lazy } from 'react';
 import { FontSizeOutlined } from '@ant-design/icons';
+import ArticleEdit from '@web/pages/article/edit';
+import { articleDetail } from '@web/api';
 
 const Article = lazy(() => import('@web/pages/article'));
 const ArticlePush = lazy(() => import('@web/pages/article/push'));
@@ -15,6 +17,19 @@ export const ArticleRouter: RouteItem = {
       path: 'push',
       element: <ArticlePush />,
       hide: true,
+    },
+    {
+      label: '编辑',
+      path: 'edit/:id',
+      element: <ArticleEdit />,
+      hide: true,
+      async loader({ params }) {
+        const { id } = params;
+        const detail = await articleDetail(id!);
+        const { title, subTitle, intro, tags, status } = detail;
+        const initData = { title, subTitle, intro, tags, status };
+        return { detail, initData };
+      },
     },
   ],
 };
