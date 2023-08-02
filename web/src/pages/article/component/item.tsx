82809608478160efde93352e37aa4c1f4e1cf25f
atoz <523333404@qq.com>
2023-08-02 21:06:48 +0800

    🚲 笔记列表项

--- web/src/pages/article/component/item.tsx
+++ web/src/pages/article/component/item.tsx
@@ -1,27 +1,105 @@
-import { Button, List } from 'antd';
+import { Button, List, Tag, message, Popconfirm } from 'antd';
+import { Link } from 'react-router-dom';
+import AppImage from '@web/components/image';
+import { articleEdit, articleRemove } from '@web/api';
+import { tailErr } from '@web/helper';
+import { useState } from 'react';
+
+/**
+ * 切换笔记状态
+ * @param status
+ */
+const switchArticleStatu = async (
+  id: number,
+  status: 0 | 1,
+  setStatu: React.Dispatch<React.SetStateAction<number>>,
+) => {
+  try {
+    message.loading({ content: '努力处理中...' });
+    await articleEdit(id, { status });
+    message.destroy();
+    message.success('操作成功');
+    setStatu(status);
+  } catch (err: unknown) {
+    tailErr(err);
+  }
+};
+
+/**
+ * 删除笔记
+ */
+const removeArticle = async (
+  id: number,
+  switchShow: React.Dispatch<React.SetStateAction<boolean>>,
+) => {
+  try {
+    message.loading({ content: '正在删除...' });
+    await articleRemove(id);
+    message.destroy();
+    message.success('操作成功');
+    switchShow(false);
+  } catch (err: unknown) {
+    tailErr(err);
+  }
+};
+
 const ArticleItem = ({ detail }: { detail: any }) => {
+  const [statu, setStatu] = useState<number>(detail.status);
+  const [show, switchShow] = useState<boolean>(true);
+  const tabs = (detail.tags ?? '')
+    .split(',')
+    .filter((item: string) => item)
+    .map((i: string) => <Tag key={i}>{i}</Tag>);
+
   return (
-    <List.Item
-      key={detail.title}
-      actions={[
-        <Button type={'link'}>编辑</Button>,
-        <Button type={'link'}>发布</Button>,
-        <Button type={'link'}>删除</Button>,
-      ]}
-      extra={
-        <img
-          width={272}
-          alt="logo"
-          src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+    show && (
+      <List.Item
+        key={detail.id}
+        actions={[
+          <Link to={`edit/${detail.id}`}>编辑</Link>,
+          statu ? (
+            <Button
+              type={'link'}
+              onClick={() => switchArticleStatu(detail.id, 0, setStatu)}
+            >
+              设为草稿
+            </Button>
+          ) : (
+            <Button
+              type={'link'}
+              onClick={() => switchArticleStatu(detail.id, 1, setStatu)}
+            >
+              发布
+            </Button>
+          ),
+          <Popconfirm
+            title="温馨提醒"
+            description="是否删除该笔记？"
+            onConfirm={() => removeArticle(detail.id, switchShow)}
+            okText="确认"
+            cancelText="取消"
+          >
+            <Button type={'link'} danger>
+              删除
+            </Button>
+          </Popconfirm>,
+        ]}
+        extra={
+          <AppImage
+            src={detail.cover?.thumb ?? detail.cover.path}
+            height={120}
+            color={detail.color}
+            preview={{ src: detail.cover.path }}
+          />
+        }
+      >
+        <List.Item.Meta
+          title={detail.subTitle + '/' + detail.title}
+          description={tabs}
         />
-      }
-    >
-      <List.Item.Meta
-        title={detail.subTitle + '/' + detail.title}
-        description={detail.tags}
-      />
-      {detail.intro}
-    </List.Item>
+        {detail.intro}
+      </List.Item>
+    )
   );
 };
 
