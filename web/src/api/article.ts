9f1a927c554856705464920d37e00262c2e1df0a
atoz <523333404@qq.com>
2023-08-02 21:01:21 +0800

    🚲 笔记相关接口

--- web/src/api/article.ts
+++ web/src/api/article.ts
@@ -15,6 +15,29 @@ export type ArticleSimpleList = Array<{
   pubDate: string;
   createDate: string;
 }>;
+export type ArticleProp = {
+  title: string;
+  subTitle: string;
+  intro: string;
+  cover: number;
+  tags: string;
+  status: 0 | 1;
+  content: string;
+  pubDate: string;
+  deleteDate?: string;
+};
+export type ArticleDetailProp = ArticleProp & {
+  id: number;
+  cover: {
+    id: number;
+    path: string;
+    thumb?: string;
+    width: number;
+    height: number;
+    size: number;
+    color: string;
+  };
+};
 export const articleList = (params: {
   start?: number;
   end?: number;
@@ -28,17 +51,37 @@ export const articleList = (params: {
 /**
  * 添加笔记
  */
-export const articleAdd = (data: {
-  title: string;
-  subTitle: string;
-  intro: string;
-  cover: number;
-  tags: string;
-  pubDate: string;
-  status: 0 | 1;
-  content: string;
-}) => {
+export const articleAdd = (data: ArticleProp) => {
   const url = '/article/v1/add';
   const method = 'POST';
   return request<unknown>({ url, method, data });
+};
+
+/**
+ * 笔记详情
+ */
+export const articleDetail = (id: string | number) => {
+  const url = '/article/v1/detail';
+  return request<ArticleDetailProp>({ url, params: { id } });
+};
+
+/**
+ * 编辑笔记
+ */
+export const articleEdit = (
+  id: string | number,
+  data: Partial<ArticleProp>,
+) => {
+  const url = '/article/v1/edit';
+  const method = 'PATCH';
+  return request<unknown>({ url, method, data: { ...data, id } });
+};
+
+/**
+ * 删除笔记
+ */
+export const articleRemove = (id: string | number) => {
+  const url = '/article/v1/remove';
+  const method = 'PATCH';
+  return request<unknown>({ url, method, params: { id } });
 };
