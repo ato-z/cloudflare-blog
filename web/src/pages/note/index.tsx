32f7f9a628b1c78d6c1dbf37084c6cc817988a59
atoz <523333404@qq.com>
2023-08-03 20:11:34 +0800

    🚲 小记模块

--- web/src/pages/note/index.tsx
+++ web/src/pages/note/index.tsx
@@ -1,5 +1,83 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Button } from 'antd';
+import { AnimaView } from '@web/components/animaRouter';
+import { useSearchForm } from '@web/components/tableView/searchForm';
+import { useTableList } from '@web/helper/useTableList';
+import { NoteProp, noteAdd, noteEdit, noteList } from '@web/api';
+import { siteConfig } from '@web/config';
+import { PushNoteModal } from './component/pushNoteModal';
+import { noteSearchProps, noteColumn } from './vars';
+
+const { pageSize } = siteConfig;
+
 const NoteList = () => {
-  return <>小记</>;
+  const [SearchForm, searchData] = useSearchForm({
+    items: noteSearchProps,
+  });
+
+  const [row, setRow] = useState({} as NoteProp & { id: number });
+  const [pushVisible, setPushVisible] = useState(false);
+  const [editVisible, setEditVisible] = useState(false);
+
+  const onTap = useCallback(
+    (action: 'edit' | 'reload', row: any) => {
+      if (action === 'edit') {
+        setRow(row);
+        setEditVisible(true);
+      }
+
+      if (action === 'reload') {
+        setParams({});
+      }
+    },
+    [setRow, setEditVisible],
+  );
+
+  const [setParams, tableView] = useTableList({
+    columns: noteColumn(onTap),
+    request: noteList,
+    pageSize,
+  });
+
+  // 用户检索状态
+  useEffect(() => {
+    setParams(searchData);
+  }, [searchData]);
+
+  return (
+    <AnimaView>
+      <article>
+        <section>{SearchForm}</section>
+        <section style={{ margin: '20px 0 ' }}>
+          <Button type={'primary'} onClick={() => setPushVisible(true)}>
+            添加小记
+          </Button>
+        </section>
+
+        <section>{tableView}</section>
+
+        {/* 新增弹窗 */}
+        <PushNoteModal
+          title="添加小记"
+          requset={async o => {
+            await noteAdd(o);
+            setParams({});
+          }}
+          open={pushVisible}
+          setOpen={setPushVisible}
+        />
+
+        {/* 编辑弹窗 */}
+        <PushNoteModal
+          title="编辑小记"
+          requset={noteEdit}
+          open={editVisible}
+          setOpen={setEditVisible}
+          initData={row}
+        />
+      </article>
+    </AnimaView>
+  );
 };
 
 export default NoteList;
---
+++ web/src/pages/note/vars.tsx
@@ -0,0 +1,97 @@
+import { noteRemove } from '@web/api';
+import { Button, Input, Popconfirm, Space } from 'antd';
+import { ColumnsType } from 'antd/es/table';
+
+/**
+ * 小记列表检索项
+ */
+export const noteSearchProps = [
+  { name: 'title', label: '标题', element: <Input /> },
+  { name: 'tags', label: '标签', element: <Input /> },
+];
+
+/**
+ * 表格项
+ */
+export const noteColumn: (
+  tap: (action: 'edit' | 'reload', row: any) => void,
+) => ColumnsType<DataType> = tap => [
+  {
+    title: 'id',
+    dataIndex: 'id',
+    key: 'id',
+  },
+  {
+    title: '标题',
+    dataIndex: 'title',
+    key: 'title',
+  },
+  {
+    title: '标签',
+    dataIndex: 'tags',
+    key: 'tags',
+  },
+  {
+    title: '内容',
+    dataIndex: 'content',
+    key: 'content',
+  },
+  {
+    title: '创建时间',
+    dataIndex: 'createDate',
+    key: 'createDate',
+  },
+  {
+    title: '操作',
+    key: 'action',
+    render: row => (
+      <Space size="middle">
+        <Popconfirm
+          title="温馨提醒"
+          description="是否删除该小记？"
+          onConfirm={async () => {
+            await noteRemove(row.id);
+            tap('reload', row);
+          }}
+          okText="确认"
+          cancelText="取消"
+        >
+          <Button type={'link'} danger>
+            删除
+          </Button>
+        </Popconfirm>
+        <Button type={'link'} onClick={() => tap('edit', row)}>
+          编辑
+        </Button>
+      </Space>
+    ),
+  },
+];
+
+type NoteProp = {
+  markdown: JSX.Element;
+};
+/**
+ * 添加/编辑小记
+ */
+export const noteProps = ({ markdown }: NoteProp) => {
+  return [
+    {
+      label: '标题',
+      name: 'title',
+      element: <Input />,
+      rules: [{ required: true }],
+    },
+    {
+      label: '标签',
+      name: 'tags',
+      element: <Input.TextArea />,
+      rules: [{ required: true }],
+    },
+    {
+      label: '内容',
+      name: 'content',
+      element: markdown,
+    },
+  ];
+};
