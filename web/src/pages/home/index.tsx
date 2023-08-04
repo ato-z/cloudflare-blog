bdcc80d006fcca5f9b27ab4a93702aaea067d6a9
atoz <523333404@qq.com>
2023-08-04 20:49:25 +0800

    🚲 首页加载动画

--- web/src/pages/home/index.tsx
+++ web/src/pages/home/index.tsx
@@ -1,7 +1,177 @@
 import { AnimaView } from '@web/components/animaRouter';
+import './index.scss';
 export const Home = () => (
   <AnimaView>
-    <div>首页</div>;
+    <article className="home-view">
+      <div className="container">
+        <div className="lamb a a-3"></div>
+        <div className="chandelier">
+          <div className="chandelier-case a a-3"></div>
+          <div className="chandelier-details">
+            <div className="chandelier-detail a a-2"></div>
+            <div className="chandelier-detail a a-2"></div>
+            <div className="chandelier-detail a a-2"></div>
+            <div className="chandelier-detail a a-2"></div>
+            <div className="line a"></div>
+          </div>
+        </div>
+        <div className="container-back cont a-4"></div>
+        <div className="container-wrapper cont a-4">
+          <div className="way"></div>
+          <div className="moon"></div>
+          <div className="star-container">
+            <div className="stars">
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+            </div>
+            <div className="stars">
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+              <div className="star"></div>
+            </div>
+          </div>
+          <div className="night-wrapper">
+            <div className="night">
+              <div className="train">
+                <div className="train-link"></div>
+                <div className="train-coach">
+                  <div className="train-mid">
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                  </div>
+                  <div className="door"></div>
+                  <div className="rollers">
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="train">
+                <div className="train-link"></div>
+                <div className="train-coach">
+                  <div className="train-mid">
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                  </div>
+                  <div className="door"></div>
+                  <div className="rollers">
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="night">
+              <div className="train">
+                <div className="train-link"></div>
+                <div className="train-coach">
+                  <div className="train-mid">
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                  </div>
+                  <div className="door"></div>
+                  <div className="rollers">
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                  </div>
+                </div>
+              </div>
+              <div className="train">
+                <div className="train-link"></div>
+                <div className="train-coach">
+                  <div className="train-mid">
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                    <div className="window">
+                      <div className="window-line"></div>
+                    </div>
+                  </div>
+                  <div className="door"></div>
+                  <div className="rollers">
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                    <div className="roller"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
   </AnimaView>
 );
 
