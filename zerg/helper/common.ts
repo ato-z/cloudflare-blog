151cc3f9d5de301e9305315e9b8e186e96b0d2bf
atoz <523333404@qq.com>
2023-08-05 17:35:21 +0800

    🏍️  细节调整

--- zerg/helper/common.ts
+++ zerg/helper/common.ts
@@ -1,4 +1,6 @@
 import { siteConfig } from '@web/config';
 const { staticDomain } = siteConfig;
-export const codeOssPath = (path: string) =>
-  `${staticDomain}/${path}`.replace(/\/+/g, '/');
+export const codeOssPath = (path: string) => {
+  const url = `${staticDomain}/${path}`.replace(/\/+/g, '/');
+  return `https://${url}`;
+};
